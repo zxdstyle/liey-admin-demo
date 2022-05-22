357120888bd32c3fcd16d0bb3ff74dc930e17a3e
zxdstyle <1430822515@qq.com>
2022-05-22 23:29:35 +0800

    menu

--- web/src/components/basic/form.bak/index.ts
+++
@@ -1,5 +0,0 @@
-import BasicForm from './src/BasicForm.vue';
-import useForm from './src/hooks/useForm';
-import ApiSwitch from './src/components/ApiSwitch.vue';
-
-export { BasicForm, useForm, ApiSwitch };
